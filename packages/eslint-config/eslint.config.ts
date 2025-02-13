be4e76de1755caca9a1f7af9e60ff3d949e2d54b
Rocket Science B.V <email@rocketsciencebv.nl>
2025-02-13 19:49:55 +0100

    fix: ci - add globals package

--- packages/eslint-config/eslint.config.ts
+++ packages/eslint-config/eslint.config.ts
@@ -1,9 +1,13 @@
+import globals from "globals";
 import js from "@eslint/js";
 import type { Linter } from "eslint";
 
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      ...globals
+    },
     rules: {
       "no-console": [0],
     },
